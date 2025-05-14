aca2371a7555e4f2bc5836849bd5872cca984e05
ibrahyma <i.missoma@cfa-insta.fr>
2025-05-15 01:10:58 +0200

    Mise à jour du Bureau : possibilité d'afficher un composant par dessus le bureau

--- src/app/pages/desktop/desktop.component.ts
+++ src/app/pages/desktop/desktop.component.ts
@@ -1,75 +1,100 @@
-import { Component } from '@angular/core';
+import {Component, ComponentRef, OnInit, Type, ViewChild, ViewContainerRef} from '@angular/core';
 import {TaskbarComponent} from "../../core/components/taskbar/taskbar.component";
 import {
-  CustomableFolderIconComponent
+    CustomableFolderIconComponent
 } from "../../shared/components/customable-folder-icon/customable-folder-icon.component";
+import {AboutComponent} from "../windows/about/about.component";
+import {CdkDrag} from "@angular/cdk/drag-drop";
 
 @Component({
-  selector: 'app-desktop',
-  imports: [
-    TaskbarComponent,
-    CustomableFolderIconComponent
-  ],
-  templateUrl: './desktop.component.html',
-  styleUrl: './desktop.component.scss'
+    selector: 'app-desktop',
+    imports: [
+        TaskbarComponent,
+        CustomableFolderIconComponent,
+        CdkDrag
+    ],
+    templateUrl: './desktop.component.html',
+    styleUrl: './desktop.component.scss'
 })
-export class DesktopComponent {
-  shortcuts: ShortcutWindow[] = [
-    {
-      name: 'À propos',
-      color: '#88CCFF',
-      shadowColor: '#224488',
-      action: () => {
-        console.log('À propos action triggered');
-      }
-    },
-    {
-      name: 'Compétences',
-      color: '#FFCC88',
-      shadowColor: '#442211',
-      action: () => {
-        console.log('Compétences action triggered');
-      }
-    },
-    {
-      name: 'Formation',
-      color: '#80E880',
-      shadowColor: '#116611',
-      action: () => {
-        console.log('Formation action triggered');
-      }
-    },
-    {
-      name: 'Expérience',
-      color: '#FF6060',
-      shadowColor: '#221111',
-      action: () => {
-        console.log('Expérience action triggered');
-      }
-    },
-    {
-      name: 'Projets',
-      color: '#CC88FF',
-      shadowColor: '#441122',
-      action: () => {
-        console.log('Projets action triggered');
-      }
-    },
-    {
-      name: 'Contact',
-      color: '#FFA858',
-      shadowColor: '#442211',
-      action: () => {
-        console.log('Contact action triggered');
-      }
-    }
-  ];
+export class DesktopComponent implements OnInit {
+    @ViewChild("window", {read: ViewContainerRef, static: true})
+    window!: ViewContainerRef;
 
+    activeWindow?: ComponentRef<any>;
+
+    ngOnInit() {
+        this.openWindow(AboutComponent)
+    }
+
+    shortcuts: ShortcutWindow[] = [
+        {
+            name: 'À propos',
+            id: 'about',
+            color: '#88CCFF',
+            shadowColor: '#224488',
+            action: () => this.openWindow(AboutComponent)
+        },
+        {
+            name: 'Compétences',
+            id: 'competences',
+            color: '#FFCC88',
+            shadowColor: '#442211',
+            action: function () {
+                console.log(`${this.id} action triggered`);
+            }
+        },
+        {
+            name: 'Formation',
+            id: 'formation',
+            color: '#80E880',
+            shadowColor: '#116611',
+            action: function () {
+                console.log(`${this.id} action triggered`);
+            }
+        },
+        {
+            name: 'Expérience',
+            id: 'experience',
+            color: '#FF6060',
+            shadowColor: '#221111',
+            action: function () {
+                console.log(`${this.id} action triggered`);
+            }
+        },
+        {
+            name: 'Projets',
+            id: 'projets',
+            color: '#CC88FF',
+            shadowColor: '#441122',
+            action: function () {
+                console.log(`${this.id} action triggered`);
+            }
+        },
+        {
+            name: 'Contact',
+            id: 'contact',
+            color: '#FFA858',
+            shadowColor: '#442211',
+            action: function () {
+                console.log(`${this.id} action triggered`);
+            }
+        }
+    ];
+
+
+    private openWindow(component: Type<any>) {
+        if (this.activeWindow) {
+            this.activeWindow.destroy();
+        }
+
+        this.activeWindow = this.window.createComponent(component);
+    }
 }
 
 interface ShortcutWindow {
-  name: string;
-  color: string;
-  shadowColor: string;
-  action: () => void;
+    id: string;
+    name: string;
+    color: string;
+    shadowColor: string;
+    action: () => any;
 }
