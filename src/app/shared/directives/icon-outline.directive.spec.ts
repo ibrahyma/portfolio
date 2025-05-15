4dc53cccdcd39e251bb54cf2d6660029441f184d
ibrahyma <i.missoma@cfa-insta.fr>
2025-05-15 18:53:02 +0200

    Création d'une directive icon-outlined

---
+++ src/app/shared/directives/icon-outline.directive.spec.ts
@@ -0,0 +1,8 @@
+import { IconOutlineDirective } from './icon-outline.directive';
+
+describe('IconOutlineDirective', () => {
+  it('should create an instance', () => {
+    const directive = new IconOutlineDirective();
+    expect(directive).toBeTruthy();
+  });
+});
