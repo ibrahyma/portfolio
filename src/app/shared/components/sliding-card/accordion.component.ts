2494a9d05e78372c4c858003b7a5f98b4c0f4ffa
ibrahyma <i.missoma@cfa-insta.fr>
2025-05-16 00:47:41 +0200

    Création du composant Accordion

---
+++ src/app/shared/components/sliding-card/accordion.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input} from '@angular/core';
+import {
+    MatAccordion,
+    MatExpansionPanel,
+    MatExpansionPanelHeader
+} from "@angular/material/expansion";
+import {MatIcon} from "@angular/material/icon";
+import {IconOutlineDirective} from "../../directives/icon-outline.directive";
+
+@Component({
+    selector: 'im-accordion',
+    imports: [
+        MatAccordion,
+        MatExpansionPanel,
+        MatExpansionPanelHeader,
+        MatIcon,
+        IconOutlineDirective
+    ],
+    templateUrl: './accordion.component.html',
+    styleUrl: './accordion.component.scss'
+})
+export class AccordionComponent {
+
+    @Input() headerText!: string;
+    @Input() materialIconName?: string;
+    @Input('icon-outlined') iconOutlined?: string;
+
+}
