c7e81f711aa0e2aaa1ea4988d76f90015a3873fc
ibrahyma <i.missoma@cfa-insta.fr>
2025-05-22 02:06:39 +0200

    Refactoring du code de School et implémentation de Experiences

--- src/app/pages/windows/experiences/experiences.component.ts
+++ src/app/pages/windows/experiences/experiences.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {AccordionComponent} from "../../../shared/components/accordion/accordion.component";
 
 @Component({
   selector: 'app-experiences',
-  imports: [],
+    imports: [
+        AccordionComponent
+    ],
   templateUrl: './experiences.component.html',
   styleUrl: './experiences.component.scss'
 })
