6e5f3d8475b2afc76e39f226c87ff266b12dcf12
ibrahyma <i.missoma@cfa-insta.fr>
2025-05-30 19:09:12 +0200

    Implémentation du composant Projects

--- src/app/pages/windows/projects/projects.component.ts
+++ src/app/pages/windows/projects/projects.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {AccordionComponent} from "../../../shared/components/accordion/accordion.component";
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [
+    AccordionComponent
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
